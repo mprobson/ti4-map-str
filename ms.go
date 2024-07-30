20d613bba39c9a739ba015afd9f62a6a125585ab
Michael Robson <mrobson@smith.edu>
2024-07-30 19:55:40 -0400

    Count through and replace zeros, initial but going to just switch indices

--- ms.go
+++ ms.go
@@ -8,9 +8,16 @@ import (
 func main() {
   mapStr := os.Args[1:]
 
+  countZeros := 0
   for index, tile := range mapStr {
     if tile == "0" {
-      fmt.Println("Found a zero! at", index)
+      if countZeros == 0 {
+        mapStr[index] = "86A0"
+      } else if countZeros == 1 {
+        mapStr[index] = "88A0"
+      }
+
+      countZeros++
     }
   }
 
